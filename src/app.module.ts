69464990a59295370be3151cb7df88094b479bd3
Mohammed alagha <modyagha7@gmail.com>
2022-12-13 15:00:28 +0200

    create the app modules and its servers and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './modules/db/database.module';
+import { UserModule } from './modules/user/user.module';
+import { OrderModule } from './modules/order/order.module';
+import { AddressModule } from './modules/address/address.module';
+import { NotificationModule } from './modules/notification/notification.module';
 import configurationFile from 'config';
 @Module({
   imports: [
-    DatabaseModule,
     ConfigModule.forRoot({
       load: [configurationFile],
       isGlobal: true,
     }),
+    DatabaseModule,
+    UserModule,
+    OrderModule,
+    AddressModule,
+    NotificationModule,
   ],
   controllers: [],
   providers: [],
