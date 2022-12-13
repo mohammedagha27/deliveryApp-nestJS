ecc1ce59b6150c2b03107b694338faee32aceffc
Mohammed alagha <modyagha7@gmail.com>
2022-12-13 10:44:11 +0200

    crete database module and provide it

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DbModule } from './modules/db/db.module';
+import { Database } from './modules/db/database/database';
+import { DatabaseModule } from './modules/db/database.module';
 
 @Module({
-  imports: [],
+  imports: [DbModule, DatabaseModule],
   controllers: [],
-  providers: [],
+  providers: [Database],
 })
 export class AppModule {}
